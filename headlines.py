6c627a16c65b87ae4f63af7a28a0d13a63a2e428
Sachin <sachin.nicky@gmail.com>
2016-07-28 00:07:41 +0530

    Added template logic and input

--- headlines.py
+++ headlines.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import feedparser
 
 app = Flask(__name__)
 
 RSS_FEEDS = {
 	"bbc" : "http://feeds.bbci.co.uk/news/rss.xml",
+	"cnn": "http://rss.cnn.com/rss/edition.rss",
 	"screen": "http://screenrant.com/feed/"
 }
 # BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml",
@@ -22,8 +23,13 @@ RSS_FEEDS = {
 # 	return get_news('screen')
 
 @app.route('/')
-@app.route('/<publication>')
-def get_news(publication="bbc"):
+# @app.route('/<publication>')
+def get_news():
+	query = request.args.get("publication")
+	if not query or query.lower() not in RSS_FEEDS:
+		publication = "bbc"
+	else:
+		publication = query.lower()
 	feed = feedparser.parse(RSS_FEEDS[publication])
 	# first_article = feed['entries'][0]
 	return render_template("home.html",articles=feed["entries"])
